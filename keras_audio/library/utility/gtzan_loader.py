c5f3678266ca53ec2d0190217399ef08f007a43b
Te Meng <tmeng94dev@gmail.com>
2018-11-14 15:18:39 -0800

    Setup music/speech classification

--- keras_audio/library/utility/gtzan_loader.py
+++ keras_audio/library/utility/gtzan_loader.py
@@ -7,8 +7,13 @@ from keras_audio.library.utility.download_utils import reporthook
 gtzan_labels = {0: 'blues', 1: 'classical', 2: 'country', 3: 'disco', 4: 'hiphop', 5: 'jazz', 6: 'metal',
                 7: 'pop', 8: 'reggae', 9: 'rock'}
 
+gtzan_music_speech_labels = {0: 'music', 1: 'speech'}
 
 def download_gtzan_music_speech(data_dir_path):
+    flag_file = os.path.join(data_dir_path, 'music_speech_done.txt')
+    if os.path.exists(flag_file):
+        return
+
     zip_file_path = data_dir_path + '/music_speech.tar.gz'
 
     if not os.path.exists(zip_file_path):
@@ -20,6 +25,9 @@ def download_gtzan_music_speech(data_dir_path):
     tar = tarfile.open(zip_file_path, "r:gz")
     tar.extractall(data_dir_path)
     tar.close()
+
+    with open(flag_file, 'wt') as file:
+        file.write('done')
 
 
 def download_gtzan_genres_if_not_found(data_dir_path):
